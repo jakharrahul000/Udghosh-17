38c634751661335d09132fc2d79de7bebbff44cc
hitesh1157 <hiteshkr.git@gmail.com>
2017-08-15 19:01:30 +0530

    Update modal

--- Register/login.php
+++ Register/login.php
@@ -2,28 +2,59 @@
 session_start();
 	require 'dbconfig/config.php';
 
-function SignIn() 
-{ 
-session_start(); //starting the session for user profile page 
-if(!empty($_POST['user'])) //checking the 'user' name which is from Sign-In.html, is it empty or have some text 
-{ 
-	$query = mysql_query("SELECT * FROM WebsiteUsers where userName = '$_POST[user]' AND pass = '$_POST[pass]'") or die(mysql_error()); 
-	$row = mysql_fetch_array($query) or die(mysql_error()); 
-	if(!empty($row['userName']) AND !empty($row['pass'])) 
-		{ 
-			$_SESSION['userName'] = $row['pass'];
-			header("location:user.html");
-		} 
-		else 
-		{ 
-			echo "SORRY... YOU ENTERD WRONG ID AND PASSWORD... PLEASE RETRY..."; 
-		} 
-} 
-} 
-if(isset($_POST['submit'])) 
-	{ 
-		SignIn(); 
-	} 
 
+function SignIn()
+{
+
+	//echo "yoyo !!";
+//session_start(); //starting the session for user profile page
+if(!empty($_POST['user'])) //checking the 'user' name which is from Sign-In.html, is it empty or have some text
+{
+	//echo ' <br> connect to DB';
+	//echo '<br>'.$_POST[user];
+	//echo '<br>'.$_POST[pass];
+
+
+	$query = mysql_query("SELECT * FROM WebsiteUsers where userName = '$_POST[user]' AND pass = '$_POST[pass]'") ;//or die(mysql_error());
+	$row = mysql_fetch_array($query);// or die(mysql_error());
+
+	//echo die("incoorrct",mysql_error());
+	//echo '<br>'.$row['userName'];
+	//echo '<br>'.$row['pass'];
+
+	if(!empty($row['userName']) AND !empty($row['pass']))
+		{
+			$_SESSION['fullName'] = $row['fullname'];
+			$_SESSION['college'] = $row['college'];
+			$_SESSION['userName'] = $row['userName'];
+
+			setcookie(fullName,$row['fullName']);
+			setcookie(college,$row['college']);
+			setcookie(userName,$row['userName']);
+
+
+			echo "<script>setTimeout(\"location.href = 'user.html';\",1);</script>";
+			echo "<script>alert(\"Welcome ".$_SESSION['fullName']." from ".$_SESSION['college']." college\");</script>";
+
+
+		}
+		else
+		{
+			//echo "SORRY... YOU ENTERD WRONG ID AND PASSWORD... PLEASE RETRY...";
+			echo '<script>alert("Enter correct username and password.");</script>';
+			echo "<script>setTimeout(\"location.href = 'login.html';\",1500);</script>";
+
+			//$_SESSION['message'] = "Enter correct userName and password!!";
+			//echo $_SESSION[message];
+			//header("location:login.html");
+
+		}
+}
+
+}
+if(isset($_POST['submit']) && !empty($_POST['submit']))
+	{
+		//echo "entered";
+		SignIn();
+	}
 ?>
-
--- Register/user.php
+++ Register/user.php
@@ -1,16 +1,16 @@
 <?php
-session_start();
 	require 'dbconfig/config.php';
-
-function NewUser() 
-{ 
-	$username = $_POST['user']; 
-	$college = $_POST['college'];
+	session_start();
+	//echo $_SESSION['userName'];
+function NewUser()
+{
+	$username = $_SESSION['userName'];
+	$college = $_SESSION['college'];
 	$sports = $_POST['sports'];
 	$gender = $_POST['gender'];
 	$captain = $_POST['captain'];
-	$email = $_POST['email']; 
-	$contact = $_POST['contact']; 
+	$email = $_POST['email'];
+	$contact = $_POST['contact'];
 	$team_size = $_POST['team_size'];
 	$player1 = $_POST['player1']; $contact1 = $_POST['contact1'];
 	$player2 = $_POST['player2']; $contact2 = $_POST['contact2'];
@@ -43,31 +43,43 @@ function NewUser()
 	$player29 = $_POST['player29']; $contact29 = $_POST['contact29'];
 	$player30 = $_POST['player30']; $contact30 = $_POST['contact30'];
 
-	$query = "INSERT INTO sports_form (username,college,sports,gender,captain,email,contact,team_size,player1,contact1,player2,contact2,player3,contact3,player4,contact4,player5,contact5,player6,contact6,player7,contact7,player8,contact8,player9,contact9,player10,contact10,player11,contact11,player12,contact12,player13,contact13,player14,contact14,player15,contact15,player16,contact16,player17,contact17,player18,contact18,player19,contact19,player20,contact20,player21,contact21,player22,contact22,player23,contact23,player24,contact24,player25,contact25,player26,contact26,player27,contact27,player28,contact28,player29,contact29,player30,contact30) VALUES ('$username','$college','$sports','$gender','$captain','$email','$contact','$team_size','$player1','$contact1','$player2','$contact2','$player3','$contact3','$player4','$contact4','$player5','$contact5','$player6','$contact6','$player7','$contact7','$player8','$contact8','$player9','$contact9','$player10','$contact10','$player11','$contact11','$player12','$contact12','$player13','$contact13','$player14','$contact14','$player15','$contact15','$player16','$contact16','$player17','$contact17','$player18','$contact18','$player19','$contact19','$player20','$contact20','$player21','$contact21','$player22','$contact22','$player23','$contact23','$player24','$contact24','$player25','$contact25','$player26','$contact26','$player27','$contact27','$player28','$contact28','$player29','$contact29','$player30','$contact30')"; 
-	$data = mysql_query ($query)or die(mysql_error()); 
-	if($data) 
-		{ 
-			echo "YOUR REGISTRATION IS COMPLETED..."; 
-		} 
-	} 
 
-function SignUp() 
-{ 
-	if(!empty($_POST['user'])) //checking the 'user' name which is from Sign-Up.html, is it empty or have some text 
-{ 
-	$query = mysql_query("SELECT * FROM sports_form WHERE username = '$_POST[user]' AND college = '$_POST[college]'") or die(mysql_error()); 
-	if(!$row = mysql_fetch_array($query) or die(mysql_error())) 
-		{ 
-			NewUser(); 
-		} 
-		else 
-		{ 
-			echo "SORRY...YOU ARE ALREADY REGISTERED USER..."; 
-		} 
-} 
-} 
-if(isset($_POST['submit'])) 
-	{ 
-		SignUp(); 
-	} 
+$query = "INSERT INTO sports_form (username,college,sports,gender,captain,email,contact,team_size,player1,contact1,player2,contact2,player3,contact3,player4,contact4,player5,contact5,player6,contact6,player7,contact7,player8,contact8,player9,contact9,player10,contact10,player11,contact11,player12,contact12,player13,contact13,player14,contact14,player15,contact15,player16,contact16,player17,contact17,player18,contact18,player19,contact19,player20,contact20,player21,contact21,player22,contact22,player23,contact23,player24,contact24,player25,contact25,player26,contact26,player27,contact27,player28,contact28,player29,contact29,player30,contact30) VALUES ('$username','$college','$sports','$gender','$captain','$email','$contact','$team_size','$player1','$contact1','$player2','$contact2','$player3','$contact3','$player4','$contact4','$player5','$contact5','$player6','$contact6','$player7','$contact7','$player8','$contact8','$player9','$contact9','$player10','$contact10','$player11','$contact11','$player12','$contact12','$player13','$contact13','$player14','$contact14','$player15','$contact15','$player16','$contact16','$player17','$contact17','$player18','$contact18','$player19','$contact19','$player20','$contact20','$player21','$contact21','$player22','$contact22','$player23','$contact23','$player24','$contact24','$player25','$contact25','$player26','$contact26','$player27','$contact27','$player28','$contact28','$player29','$contact29','$player30','$contact30')";
+
+
+	$data = mysql_query ($query)or die(mysql_error());
+	if($data)
+		{
+			echo '<script>cnf=confirm("You have successfully registered for this sport. Do you want to register for another sport?");</script>';
+			echo "<script> if(cnf){setTimeout(\"location.href = 'user.html';\",500);} else { setTimeout(\"location.href = '../index.html';\",500); }</script>";
+		}
+
+	}
+
+function SignUp()
+{
+	if(!empty($_SESSION['userName'])) //checking the 'user' name which is from Sign-Up.html, is it empty or have some text
+{
+	//check for
+	$query = mysql_query("SELECT * FROM sports_form WHERE username = '$_SESSION[userName]' AND college = '$_SESSION[college]' AND sports = '$_POST[sports]'") or die(mysql_error());
+	if(!$row = mysql_fetch_array($query))
+		{
+			NewUser();
+		}
+		else
+		{
+			echo '<script>cnf=confirm("You have already registered for this sport. Do you want to register for another sport?");</script>';
+			echo "<script> if(cnf){setTimeout(\"location.href = 'user.html';\",500);} else { setTimeout(\"location.href = '../index.html';\",500); }</script>";
+
+			//echo '<script></script>';
+			//echo "<script>setTimeout(\"location.href = 'login.html';\",1500);</script>";
+			//confirm(Do you want to register for another soprt?)
+		}
+
+}
+}
+if(isset($_POST['submit']))
+	{
+		SignUp();
+	}
 ?>
